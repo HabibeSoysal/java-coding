4191890f3f68e752e5eb2883e9b6fde0f13fdc86
HabibeSoysal <hsoysal2023@gmail.com>
2022-07-20 14:16:54 +0300

    add

--- src/day26_constructor.java
+++
@@ -1,2 +0,0 @@
-package PACKAGE_NAME;public class day26_constructor {
-}
---
+++ src/day26_constructor/ArabaRunenr.java
@@ -0,0 +1,21 @@
+import day25_constructor.Car;
+
+public class ArabaRunenr {
+    public ArabaRunenr() {
+    }
+
+    public static void main(String[] args) {
+        //bir gün önceki car claassımdam obje oluşturalım
+        Car car1=new Car();
+        System.out.println(car1.fiyat);//farklı bir classtam obje olusturdugumuzda access modifier leri dikkate almalıyı
+
+        /*
+        araba classında obje olusturdupumda default constructor kullanıldıysa
+        tum öz için tek tek deger atamak zorunda kalırız
+         */
+
+
+
+
+    }
+}
